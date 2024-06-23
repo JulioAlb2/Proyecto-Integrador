f0e93ce3b531242b6709cdbc3a9473d2790cee3f
JulioAlb453 <142369736+JulioAlb453@users.noreply.github.com>
2024-06-23 17:38:45 -0600

    version 0.1

---
+++ Futuro Igualitario/src/App.jsx
@@ -0,0 +1,20 @@
+import Button from './components/Button';
+import Header from './components/Header';
+import Notification from './components/Notification';
+import Table from './components/Table/Table';
+import './App.css';
+
+
+function App(){
+  return(
+    <>
+    <Notification/>
+      <Header/>
+      <Button/>
+      <Table/>
+    </>
+  );
+  
+}
+
+export default App
